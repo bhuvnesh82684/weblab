b77259736bf195a2b5f609671617f97491ca4bf9
Bhuvnesh <bhuvneshtenguria123@gmail.com>
2018-12-08 09:48:39 +0530

    Update 7.php

--- 7.php
+++ 7.php
@@ -1,6 +1,13 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<meta http-equiv="refresh" content="1"/>
+</head>
+<body>
 <?php
 date_default_timezone_set('Asia/kolkata');
 $n=strtotime("now");
 echo"current time is ".date("h:i:s:a",$n);
 ?>
-
+</body>
+</html>
